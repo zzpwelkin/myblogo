d6061a719bb37d244e04d7b22c4c040d9eee2047
zzpwelkin <zzpwelkin@gmail.com>
2012-08-23 22:06:57 +0800

    修改主网站网址为github

--- _config.py
+++ _config.py
@@ -19,8 +19,8 @@
 #  If you're hosting a blogofile powered site as a subdirectory of a larger
 #  non-blogofile site, then you would set the site_url to the full URL
 #  including that subdirectory: "http://www.yoursite.com/path/to/blogofile-dir"
-#site.url = "http://zzpwelkin.github.com/myblogo/"
-site.url = "http://localhost:8080/myglogo/"
+site.url = "http://zzpwelkin.github.com/myblogo/"
+#site.url = "http://localhost:8080/myglogo/"
 site.dir = "_site"
 
 #### Blog Settings ####
